d31c8e3b011042bc8880877ef7ef758859f7579c
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-22 12:54:59 +0530

    Allow multipart profile upload requests

--- client/vite-project/src/axiosCalls/axios.js
+++ client/vite-project/src/axiosCalls/axios.js
@@ -1,15 +1,9 @@
 // address - 8085
-//json
-// cookies ,credentails
+// cookies + credentials
 
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : 'http://localhost:8085/',
-    withCredentials : true,
-    headers:{
-        "Content-Type":"application/json"
-    }
-
+    baseURL: 'http://localhost:8085/',
+    withCredentials: true
 })
-
