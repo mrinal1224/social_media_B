2f77eb6f2c37f41e51e8f010b462b453cf3cdaa3
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-22 12:54:54 +0530

    Add authenticated profile update upload route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { followUser, getMe, getUserProfile, loginUser, registerUser, testUpload, unfollowUser } from '../controllers/user.controllers.js'
+import { followUser, getMe, getUserProfile, loginUser, registerUser, testUpload, unfollowUser, updateProfile } from '../controllers/user.controllers.js'
 import { isAuthenticated } from '../middlewares/authMiddleware.js'
 import upload from '../middlewares/upload.middlerware.js'
 
@@ -13,6 +13,9 @@ userRoutes.get('/profile/:username', isAuthenticated, getUserProfile)
 // Following and followers
 userRoutes.post('/:id/follow', isAuthenticated, followUser)
 userRoutes.delete('/:id/unfollow', isAuthenticated, unfollowUser)
+
+// UPDATED: Authenticated multipart endpoint for profile fields + optional profileImage.
+userRoutes.put('/profile', isAuthenticated, upload.single('profileImage'), updateProfile)
 
 userRoutes.post('/testUpload' ,  upload.single('profileImage') , testUpload)
 
